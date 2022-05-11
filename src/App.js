f94e1d9d595c3f2736933c77d2d50bc34071fba1
SAKSHAMVIPER <sakshamsinghrkt387@gmail.com>
2022-05-11 10:30:21 +0530

    movies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from './components/Main'
+import NavBar from './components/NavBar'
+function App() {
+  return (
+    <>
+    <div>APP</div>
+    <Main></Main>
+    <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Genre from './Genre'
+import Movies from './Movies'
+
+function Main() {
+  return (
+    <>
+    <div>Main</div>
+     
+    <Genre></Genre>
+    <Movies></Movies>
+
+    </>
+  )
+}
+
+export default Main
