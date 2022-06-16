b2e9186a9aa4a752387a86caab418cf3f44ccd53
SAKSHAMVIPER <sakshamsinghrkt387@gmail.com>
2022-06-16 09:48:51 +0530

    movies

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+function Genre(props) {
+  const [isLoaded, setLoaded] = useState(true);
+  const [content, setContent] = useState([]);
+  const sendGenre = (e) => {
+    console.log("genre : " + e.target.textContent)
+    props.setGlobalGenre(e.target.textContent);
+  }
+  // so i will run only one time after first execution of return statement
+  // useeffect -> outer function, async function  
+  useEffect(function () {
+    (async function () {
+      // fetch is inbuilt feature of browser that makes the request to get data -> promise based
+      let response = await fetch('https://react-backend101.herokuapp.com/genres');
+      response = await response.json();
+      // console.log(response); 
+      setLoaded(false);
+      setContent(response);
+    })();
+  }
 
-function Genre() {
+    , [])
   return (
-    <div className='mr-6' >
-genre
-    </div>
+    <div className="Genre">
+      <div className="
+      mr-6 border-2 w-40 text-center h-10 font-bold"
+        onClick={sendGenre}>All Genre</div>
+      {isLoaded == true ?
+        <div className="font-bold"> Loading...</div >
+        : content.genres.map(function (genre) {
+          return (< div
+            key={genre._id}
+            className="mr-6 border-2 w-40 text-center h-10 border-t-0 font-bold"
+            onClick={sendGenre}
+          >
+            {genre.name}</div>
+          )
+        }
+        )
+      }
+    </div >
+
+
   )
 }
 
-export default Genre 
+export default Genre
